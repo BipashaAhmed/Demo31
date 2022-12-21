4aaf1be3f537758b1cebdaa4bba7c293e99d2fb6
BipashaAhmed <bipsi910@gmail.com>
2022-12-21 17:49:35 -0500

    new commit

---
+++ src/basic/Practice1.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Practice1 {
+public static void main(String[]args) {
+		
+		String firstName="Bipasha";
+		String lastName="Ahmed";
+		String age="32";
+		System.out.println("My Full Name is : "+firstName+" "+lastName);
+		
+		
+		
+		System.out.println("My Full Name is : "+firstName+" "+lastName+" "+"\nAGE "+age);
+		
+}
+}
